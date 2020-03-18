d2b00671208fbefcaaab2b684e7083c29a08708b
THPD-Hathaikan <THPD-Hathaikan@THPDP002>
2020-03-18 09:29:05 +0700

    commit

---
+++ TrackCAPD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrackCAPD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
